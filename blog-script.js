7bda5f292df1c779b5bac80c9fe2245a8a921853
libratico <ceviche45@gmail.com>
2026-03-22 10:44:46 -0400

    cambio de link
    mejorando

--- blog-script.js
+++ blog-script.js
@@ -29,7 +29,7 @@ const articulosBlog = [
         resumen: "Aprendes cómo ganar dinero como afiliado en Hotmart con estrategias claras, actualizadas y efectivas. En Rebeldes Digitales aprenderás paso a paso cómo iniciar en el mundo del marketing de afiliados sin necesidad de experiencia previa, y construir una fuente de ingresos online desde casa.",
         imagen: "imagen/rebeldes digitales.jpg",
         fecha: "15 Marzo, 2026",
-        link: "https://go.hotmart.com/R104912472K"
+        link: "https://go.hotmart.com/R104912472K?ap=8e9f"
     }
 ];
 
